5d0daa9a27eaf76cb5ed52d16ea6b009e5cfc96b
Tom Yotwongjai <travel3.biz@gmail.com>
2016-07-16 08:21:29 +0000

    quotes_controller.rb

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,12 +1,7 @@
 class QuotesController < ApplicationController
 
 def index
-<<<<<<< HEAD
-  @quote = Quote.order("RANDOM()").first
-  end
+   @quote = Quote.order("RANDOM()").first
+   end
 
-=======
-@quote = Quote.order("RANDOM()").first
- end
->>>>>>> 172e9a3a550fe025fa59156c51174c5023d94e0a
 end
