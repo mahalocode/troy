99341b748becfc7de88230aa8a9d2df110d5ddf4
Tom Yotwongjai <travel3.biz@gmail.com>
2016-07-18 23:55:11 +0000

    modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,9 +4,7 @@ def index
    @quote = Quote.order("RANDOM()").first
    end
 
- def new
-  @quote = Quote.new
- end
+
 
  def create
   @quote = Quote.create(quote_params)
@@ -15,7 +13,7 @@ def index
   end
   redirect_to root_path
  end
- 
+
  def about
  end
 
