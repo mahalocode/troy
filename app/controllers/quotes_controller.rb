cf0c73e38750fce2325b36bd0b4f956f722bd443
Tom Yotwongjai <travel3.biz@gmail.com>
2016-07-18 21:17:42 +0000

    error message

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -9,7 +9,10 @@ def index
  end
 
  def create
-  Quote.create(quote_params)
+  @quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+  end
   redirect_to root_path
  end
 
